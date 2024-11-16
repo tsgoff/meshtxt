83f0082a3f981c00f90849e7f2bc8edd8491ca85
liamcottle <liam@liamcottle.com>
2024-11-16 20:28:47 +1300

    set vite build dir

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ export default {
     // vite app is loaded from /src
     root: path.join(__dirname, "src"),
 
+    // build to /dist instead of /src/dist
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
+
     // add plugins
     plugins: [
         vue(),
