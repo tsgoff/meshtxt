c18e32b85977d03723a8fc326653f30d0fceb8b6
liamcottle <liam@liamcottle.com>
2024-11-22 22:16:36 +1300

    hide channel psk badge text on small screens

--- src/components/channels/ChannelPskBadge.vue
+++ src/components/channels/ChannelPskBadge.vue
@@ -9,7 +9,7 @@
                         <path fill-rule="evenodd" d="M14.5 1A4.5 4.5 0 0 0 10 5.5V9H3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-1.5V5.5a3 3 0 1 1 6 0v2.75a.75.75 0 0 0 1.5 0V5.5A4.5 4.5 0 0 0 14.5 1Z" clip-rule="evenodd" />
                     </svg>
                 </div>
-                <div>No Key</div>
+                <div class="hidden sm:block">No Key</div>
             </div>
         </div>
 
@@ -21,7 +21,7 @@
                         <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
                     </svg>
                 </div>
-                <div>Default Key</div>
+                <div class="hidden sm:block">Default Key</div>
             </div>
         </div>
 
@@ -33,7 +33,7 @@
                         <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
                     </svg>
                 </div>
-                <div>8-Bit Key</div>
+                <div class="hidden sm:block">8-Bit Key</div>
             </div>
         </div>
 
@@ -45,7 +45,7 @@
                         <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
                     </svg>
                 </div>
-                <div>128-Bit Key</div>
+                <div class="hidden sm:block">128-Bit Key</div>
             </div>
         </div>
 
@@ -57,7 +57,7 @@
                         <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
                     </svg>
                 </div>
-                <div>256-Bit Key</div>
+                <div class="hidden sm:block">256-Bit Key</div>
             </div>
         </div>
 
