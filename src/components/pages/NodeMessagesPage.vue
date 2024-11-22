8bdb9342be689cd848ce3fc40b0f4c5da17f9126
liamcottle <liam@liamcottle.com>
2024-11-22 22:17:59 +1300

    hide message encryption key badge text on small screens

--- src/components/pages/NodeMessagesPage.vue
+++ src/components/pages/NodeMessagesPage.vue
@@ -15,7 +15,7 @@
                                     <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
                                 </svg>
                             </div>
-                            <div>Secure Key</div>
+                            <div class="hidden sm:block">Secure Key</div>
                         </div>
                     </div>
                     <div v-else @click="onChannelKeyInfoClick">
@@ -25,7 +25,7 @@
                                     <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
                                 </svg>
                             </div>
-                            <div>Channel Key</div>
+                            <div class="hidden sm:block">Channel Key</div>
                         </div>
                     </div>
                 </div>
