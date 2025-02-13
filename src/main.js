b48101bf58f066b9e2c66a333c8ba92aaf3b4713
liamcottle <liam@liamcottle.com>
2025-02-13 15:14:38 +1300

    preload route components

--- src/main.js
+++ src/main.js
@@ -7,92 +7,101 @@ import App from './components/App.vue';
 import GlobalState from "./js/GlobalState.js";
 import {BleConnection, Protobuf, SerialConnection} from "@meshtastic/js";
 
+const routes = [
+    {
+        name: "main",
+        path: '/',
+        component: () => import("./components/pages/MainPage.vue"),
+    },
+    {
+        name: "connect",
+        path: '/connect',
+        component: () => import("./components/pages/ConnectPage.vue"),
+    },
+    {
+        name: "connect.http",
+        path: '/connect/http',
+        component: () => import("./components/pages/ConnectViaHttpPage.vue"),
+    },
+    {
+        name: "channel.messages",
+        path: '/channels/:channelId/messages',
+        props: true,
+        component: () => import("./components/pages/ChannelMessagesPage.vue"),
+    },
+    {
+        name: "node",
+        path: '/nodes/:nodeId',
+        props: true,
+        component: () => import("./components/pages/NodePage.vue"),
+    },
+    {
+        name: "node.messages",
+        path: '/nodes/:nodeId/messages',
+        props: true,
+        component: () => import("./components/pages/NodeMessagesPage.vue"),
+    },
+    {
+        name: "node.files",
+        path: '/nodes/:nodeId/files',
+        props: true,
+        component: () => import("./components/pages/NodeFilesPage.vue"),
+    },
+    {
+        name: "node.settings",
+        path: '/nodes/:nodeId/settings',
+        props: true,
+        component: () => import("./components/pages/settings/NodeSettingsPage.vue"),
+    },
+    {
+        name: "node.settings.user",
+        path: '/nodes/:nodeId/settings/user',
+        props: true,
+        component: () => import("./components/pages/settings/NodeUserSettingsPage.vue"),
+    },
+    {
+        name: "node.settings.channels",
+        path: '/nodes/:nodeId/settings/channels',
+        props: true,
+        component: () => import("./components/pages/settings/NodeChannelsSettingsPage.vue"),
+    },
+    {
+        name: "node.settings.channels.edit",
+        path: '/nodes/:nodeId/settings/channels/:channelId',
+        props: true,
+        component: () => import("./components/pages/settings/NodeChannelSettingsPage.vue"),
+    },
+    {
+        name: "node.traceroutes",
+        path: '/nodes/:nodeId/traceroutes',
+        props: true,
+        component: () => import("./components/pages/NodeTraceRoutesPage.vue"),
+    },
+    {
+        name: "node.traceroutes.run",
+        path: '/nodes/:nodeId/traceroutes/run',
+        props: true,
+        component: () => import("./components/pages/NodeRunTraceRoutePage.vue"),
+    },
+    {
+        name: "traceroute",
+        path: '/traceroutes/:traceRouteId',
+        props: true,
+        component: () => import("./components/pages/TraceRoutePage.vue"),
+    },
+];
+
 const router = createRouter({
     history: createMemoryHistory(),
-    routes: [
-        {
-            name: "main",
-            path: '/',
-            component: () => import("./components/pages/MainPage.vue"),
-        },
-        {
-            name: "connect",
-            path: '/connect',
-            component: () => import("./components/pages/ConnectPage.vue"),
-        },
-        {
-            name: "connect.http",
-            path: '/connect/http',
-            component: () => import("./components/pages/ConnectViaHttpPage.vue"),
-        },
-        {
-            name: "channel.messages",
-            path: '/channels/:channelId/messages',
-            props: true,
-            component: () => import("./components/pages/ChannelMessagesPage.vue"),
-        },
-        {
-            name: "node",
-            path: '/nodes/:nodeId',
-            props: true,
-            component: () => import("./components/pages/NodePage.vue"),
-        },
-        {
-            name: "node.messages",
-            path: '/nodes/:nodeId/messages',
-            props: true,
-            component: () => import("./components/pages/NodeMessagesPage.vue"),
-        },
-        {
-            name: "node.files",
-            path: '/nodes/:nodeId/files',
-            props: true,
-            component: () => import("./components/pages/NodeFilesPage.vue"),
-        },
-        {
-            name: "node.settings",
-            path: '/nodes/:nodeId/settings',
-            props: true,
-            component: () => import("./components/pages/settings/NodeSettingsPage.vue"),
-        },
-        {
-            name: "node.settings.user",
-            path: '/nodes/:nodeId/settings/user',
-            props: true,
-            component: () => import("./components/pages/settings/NodeUserSettingsPage.vue"),
-        },
-        {
-            name: "node.settings.channels",
-            path: '/nodes/:nodeId/settings/channels',
-            props: true,
-            component: () => import("./components/pages/settings/NodeChannelsSettingsPage.vue"),
-        },
-        {
-            name: "node.settings.channels.edit",
-            path: '/nodes/:nodeId/settings/channels/:channelId',
-            props: true,
-            component: () => import("./components/pages/settings/NodeChannelSettingsPage.vue"),
-        },
-        {
-            name: "node.traceroutes",
-            path: '/nodes/:nodeId/traceroutes',
-            props: true,
-            component: () => import("./components/pages/NodeTraceRoutesPage.vue"),
-        },
-        {
-            name: "node.traceroutes.run",
-            path: '/nodes/:nodeId/traceroutes/run',
-            props: true,
-            component: () => import("./components/pages/NodeRunTraceRoutePage.vue"),
-        },
-        {
-            name: "traceroute",
-            path: '/traceroutes/:traceRouteId',
-            props: true,
-            component: () => import("./components/pages/TraceRoutePage.vue"),
-        },
-    ],
+    routes: routes,
 });
+
+// preload all route components, so they are available even if the server deploys a new version before the user navigates to a page
+for(const route of routes){
+    if(typeof route.component === 'function'){
+        route.component();
+    }
+}
 
 createApp(App)
     .use(router)
